5205ea7b7ffc873022b1ad890d422aa45bfd05ba
sivilabdullah <194308008@ogr.ebyu.edu.tr>
2023-06-09 13:03:16 +0300

    finish project

---
+++ shape SwiftUI/CirculShape.swift
@@ -0,0 +1,49 @@
+//
+//  CirculShape.swift
+//  shape SwiftUI
+//
+//  Created by abdullah's Ventura on 9.06.2023.
+//
+import Foundation
+import SwiftUI
+
+struct CirculShape: Shape {
+    
+    
+    var percent : Double
+    var startAngle : Double
+    typealias AnimatableData  = Double
+    var animatableData: Double {
+        get{
+            return percent
+        }
+        set{
+            percent = newValue
+        }
+    }
+    
+    init(percent: Double = 100, startAngle: Double = -90) {
+        self.percent = percent
+        self.startAngle = startAngle
+        
+        
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let width = rect.width
+        let height = rect.height
+        let radius = min(width,height) / 2
+        let center = CGPoint(x: width / 2 , y: height / 2)
+        let endAngle = (self.percent / 100 * 360) + self.startAngle
+        
+        return Path{ path in
+            path.addArc(center: center, radius: radius, startAngle: Angle(degrees: startAngle), endAngle: Angle(degrees: endAngle), clockwise: false)
+            
+        }
+    }
+    
+ 
+}
+
+
--- shape SwiftUI/ContentView.swift
+++ shape SwiftUI/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var firsCirclePercentage : Double = 0
+    @State var secondCirclePercentage : Double = 0
+    @State var thirthCirclePercentage : Double = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            
+         Circul(lineWidth: 40,
+                backgroundColor: Color.gray.opacity(0.2),
+                foregroundColor: Color.yellow.opacity(0.9),
+                percentage: firsCirclePercentage)
+         .frame(width: 450, height: 450, alignment: .center)
+         .onTapGesture {
+             if self.firsCirclePercentage == 0 {
+                 self.firsCirclePercentage = 30
+             }else{
+                 self.firsCirclePercentage = 0
+             }
+         }
+            Circul(lineWidth: 40,
+                   backgroundColor: Color.green.opacity(0.2),
+                   foregroundColor: Color.blue.opacity(0.9),
+                   percentage: thirthCirclePercentage)
+            .frame(width: 350, height: 350, alignment: .center)
+            .onTapGesture {
+                if self.thirthCirclePercentage == 0 {
+                    self.thirthCirclePercentage = 40
+                }else{
+                    self.thirthCirclePercentage = 0
+                }
+            }
+            
+            Circul(lineWidth: 40,
+                   backgroundColor: Color.red.opacity(0.2),
+                   foregroundColor: Color.purple.opacity(0.9),
+                   percentage: secondCirclePercentage)
+            .frame(width: 250, height: 250, alignment: .center)
+            .onTapGesture {
+                if self.secondCirclePercentage == 0 {
+                    self.secondCirclePercentage = 50
+                }else{
+                    self.secondCirclePercentage = 0
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
