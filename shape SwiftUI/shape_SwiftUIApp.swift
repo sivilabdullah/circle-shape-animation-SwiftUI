fc5f7d41e1831bf4bc0c83b304d5ca282c145826
sivilabdullah <194308008@ogr.ebyu.edu.tr>
2023-06-09 12:10:46 +0300

    Initial Commit

---
+++ shape SwiftUI/shape_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  shape_SwiftUIApp.swift
+//  shape SwiftUI
+//
+//  Created by abdullah's Ventura on 9.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct shape_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
